37d12499f3877d72f2ad6ba6360e7632c7e6107e
johkade <johkade.dev@gmail.com>
2023-05-14 15:55:06 +0200

    fix: falsy links

--- src/const/index.ts
+++ src/const/index.ts
@@ -4,7 +4,7 @@ export const POSTS: Post[] = [
   {
     title: "css units and what they actually mean",
     body: "Who the heck is gonna see though rem, vh, vw, px and all of that crap !? - We'll, you will after reading this article!",
-    slug: "blog/css-units-and-what-they-mean",
+    slug: "/blog/css-units-and-what-they-mean",
     category: "Web Development",
     createdAt: "2023/05/14",
     author: "me",
